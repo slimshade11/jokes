eac8e96a0ace61a7c4893a4e0510ba825b446e4d
Piotr Krzaczkowski <slimhannya@gmail.com>
2023-03-12 23:17:25 +0100

    fix(app): remove resolvers, select values from store

--- src/app/pages/jokes/jokes-view/jokes-view.component.ts
+++ src/app/pages/jokes/jokes-view/jokes-view.component.ts
@@ -1,26 +1,21 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
 import { Joke } from '@common/models/joke.model';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { map, Observable, take, takeUntil, tap } from 'rxjs';
+import { map, Observable, takeUntil } from 'rxjs';
 import { AddJokeDialogComponent } from '@jokes/components/add-joke-dialog/add-joke-dialog.component';
 import { DestroyComponent } from '@standalone/components/destroy/destroy.component';
 import { Store } from '@ngrx/store';
-import { JokesActions } from '@store/jokes';
+import { JokesActions, JokesSelectors } from '@store/jokes';
 
 @Component({
   selector: 'jokes-jokes-view',
   templateUrl: './jokes-view.component.html',
 })
 export class JokesViewComponent extends DestroyComponent {
-  public jokes$: Observable<Joke[]> = this.getJokes$();
+  public jokes$: Observable<Joke[]> = this.store.select(JokesSelectors.jokes);
 
-  constructor(private activatedRoute: ActivatedRoute, private dialogService: DialogService, private store: Store) {
+  constructor(private dialogService: DialogService, private store: Store) {
     super();
-  }
-
-  private getJokes$(): Observable<Joke[]> {
-    return this.activatedRoute.data.pipe(map(({ jokes }: Data): Joke[] => jokes));
   }
 
   public openAddJokeDialog(): void {
--- src/app/pages/my-jokes/my-jokes-view/my-jokes-view.component.ts
+++ src/app/pages/my-jokes/my-jokes-view/my-jokes-view.component.ts
@@ -5,7 +5,7 @@ import { Joke } from '@common/models/joke.model';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Observable, map, takeUntil } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { JokesActions } from '@store/jokes';
+import { JokesActions, JokesSelectors } from '@store/jokes';
 import { DestroyComponent } from '@standalone/components/destroy/destroy.component';
 import { RemoveJokeDialogComponent } from '@my-jokes/components/remove-joke-dialog/remove-joke-dialog.component';
 
@@ -15,9 +15,9 @@ import { RemoveJokeDialogComponent } from '@my-jokes/components/remove-joke-dial
   styleUrls: ['./my-jokes-view.component.scss'],
 })
 export class MyJokesViewComponent extends DestroyComponent {
-  public myJokes$: Observable<Joke[]> = this.getJokes$();
+  public myJokes$: Observable<Joke[]> = this.store.select(JokesSelectors.myJokes);
 
-  constructor(private activatedRoute: ActivatedRoute, private dialogService: DialogService, private store: Store) {
+  constructor(private dialogService: DialogService, private store: Store) {
     super();
   }
 
@@ -32,8 +32,6 @@ export class MyJokesViewComponent extends DestroyComponent {
       next: (myJoke: Joke): void => {
         if (myJoke) {
           this.store.dispatch(JokesActions.addJoke({ myJoke }));
-
-          window.location.reload();
         }
       },
     });
@@ -50,14 +48,8 @@ export class MyJokesViewComponent extends DestroyComponent {
       next: (isConfirmed): void => {
         if (isConfirmed) {
           this.store.dispatch(JokesActions.removeJoke({ jokeId }));
-
-          window.location.reload();
         }
       },
     });
-  }
-
-  private getJokes$(): Observable<Joke[]> {
-    return this.activatedRoute.data.pipe(map(({ myJokes }: Data): Joke[] => myJokes));
   }
 }
